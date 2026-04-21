dba7b8088c90968f4169d7890c17490751ea6f8d
Kaiden <11036039+KaidenP@users.noreply.github.com>
2026-04-21 04:37:50 -0400

    add hooks for decrypting private key on Linux and Windows; remove obsolete script

---
+++ .hooks/win/hook_apply_pre_00-decrypt-private-key.ps1
@@ -0,0 +1,22 @@
+$agePath = Join-Path $env:USERPROFILE ".age_identity"
+$sourceDir = "{{ .chezmoi.sourceDir }}"
+
+if (-not (Test-Path $agePath)) {
+    & chezmoi age decrypt --output $agePath --passphrase "$sourceDir/.age_identity.age"
+
+    $acl = Get-Acl $agePath
+    $acl.SetAccessRuleProtection($true, $true)
+    $acl.Access | ForEach-Object {
+        $acl.RemoveAccessRule($_)
+    }
+
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+        [System.Security.Principal.WindowsIdentity]::GetCurrent().User,
+        "FullControl",
+        "None",
+        "None",
+        "Allow"
+    )
+    $acl.AddAccessRule($rule)
+    Set-Acl $agePath $acl
+}
