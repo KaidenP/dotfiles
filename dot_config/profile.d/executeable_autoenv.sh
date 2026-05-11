23d2a865745cc1761969897472be85dd624a8c1b
Kaiden <11036039+KaidenP@users.noreply.github.com>
2026-05-11 01:12:56 -0400

    autoenv

---
+++ dot_config/profile.d/executeable_autoenv.sh
@@ -0,0 +1,11 @@
+AUTOENV_AUTH_FILE=~/.local/state/autoenv/authorized_list
+AUTOENV_NOTAUTH_FILE=~/.local/state/autoenv/not_authorized_list
+# AUTOENV_ENV_FILENAME=.env
+AUTOENV_LOWER_FIRST=true
+# AUTOENV_ENV_LEAVE_FILENAME=.env.leave
+AUTOENV_ENABLE_LEAVE=true
+# AUTOENV_ASSUME_YES=
+# AUTOENV_VIEWER=cat
+# AUTOENV_PRESERVE_CD=
+
+source ~/.autoenv/activate.sh
