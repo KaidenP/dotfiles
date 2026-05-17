a5af92fa499964c1a9199732e3ad349e02220a5d
Kaiden <11036039+KaidenP@users.noreply.github.com>
2026-05-17 14:30:23 -0400

    Refactor executable_editor.sh to ensure EDITOR is set correctly based on TERM_PROGRAM

--- dot_config/profile.d/executable_editor.sh
+++ dot_config/profile.d/executable_editor.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-
-if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-	export EDITOR='code -wr'
-else
-	export EDITOR='nano'
+if [[ "$EDITOR" != code* ]]; then
+	if [[ "$VSCODE_INJECTION" == "1" ]]; then
+		export EDITOR='code -wr'
+	else
+		export EDITOR='nano'
+	fi
 fi
